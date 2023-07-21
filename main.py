f796dde450a0a1cb6212086cb805d5c00aadc099
Raimundo Alexandre <r-alexandre-123@hotmail.com>
2023-07-21 00:58:38 -0300

    att
    projeto finalizado por enquanto

--- main.py
+++ main.py
@@ -1,32 +1,36 @@
-import xmltodict
+import xmltodict as xml
 import os
 import pandas as pd
 
+
 def pegar_infos(nome_arquivo, valores):
-    # print(f"Pegou as informações {nome_arquivo}")
-    with open(f'nfs/{nome_arquivo}', "rb") as arquivo_xml:
-        dic_arquivo = xmltodict.parse(arquivo_xml)
+    #print(f"Pegou as informações {nome_arquivo}")
+    with open(f"nfs/{nome_arquivo}", "rb") as arquivo_xml:
+        dic_arquivo = xml.parse(arquivo_xml)
 
         if "NFe" in dic_arquivo:
-            infos_nf = dic_arquivo["NFe"]['infNFe']
+            infos_nf = dic_arquivo["NFe"]["infNFe"]
         else:
-            infos_nf = dic_arquivo['nfeProc']["NFe"]['infNFe']
+            infos_nf = dic_arquivo["nfeProc"]["NFe"]["infNFe"]
+
         numero_nota = infos_nf["@Id"]
-        empresa_emissora = infos_nf['emit']['xNome']
+        empre_emisora = infos_nf["emit"]["xNome"]
         nome_cliente = infos_nf["dest"]["xNome"]
         endereco = infos_nf["dest"]["enderDest"]
-        if "vol" in infos_nf["transp"]:
+
+        if "vol" in infos_nf['transp']:
             peso = infos_nf["transp"]["vol"]["pesoB"]
         else:
-            peso = "Não informado"
-        valores.append([numero_nota, empresa_emissora, nome_cliente, endereco, peso])
+            peso = 'Não informado'
 
-lista_arquivos = os.listdir("nfs")
+        valores.append([numero_nota, empre_emisora, nome_cliente, endereco, peso])
 
-colunas = ["numero_nota", "empresa_emissora", "nome_cliente", "endereco", "peso"]
+list_arquivos = os.listdir('nfs')
+
+colunas = ["numero_nota", "empre_emisora", "nome_cliente", "endereco", "peso"]
 valores = []
-for arquivo in lista_arquivos:
+for arquivo in list_arquivos:
     pegar_infos(arquivo, valores)
 
 tabela = pd.DataFrame(columns=colunas, data=valores)
-tabela.to_excel("NotasFiscais.xlsx", index=False)
+tabela.to_excel("Notas fiscais.xlsx", index=False)
